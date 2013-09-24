ef45073a40e59f8e9a84e84c1ae2e1a0cc01f4d0
sorapark <srnice5298@gmail.com>
2013-09-24 18:38:17 +0900

    python code modify

--- map.py
+++ map.py
@@ -8,19 +8,20 @@ import simplejson as json
 import sys
 import traceback
 
-for line in iter(sys.stdin.readline, ''):
-  try:
-    line = line.rstrip()
-    lines = line.split("\t")
-    sys.stderr.write("hello world")
+if __name__ == "__main__":
+  for line in iter(sys.stdin.readline, ''):
+    try:
+      line = line.rstrip()
+      lines = line.split("\t")
+      sys.stderr.write("reporter:counter")
 
-    if len(lines) < 2:
-      continue
-    keys = lines[0]
-    data = lines[1]
-    sys.stdout.write(keys)
-  except:
-    # print line
-    sys.stderr.write("Error reading line: "+line+"\n")
-    traceback.print_exc()
+      if len(lines) < 2:
+        continue
+      keys = lines[0]
+      data = lines[1]
+      sys.stdout.write(keys)
+    except:
+      # print line
+      sys.stderr.write("Error reading line: "+line+"\n")
+      traceback.print_exc()
 
