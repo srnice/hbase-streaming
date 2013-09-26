772c0728e9dba0c303af8e458dd33e21e575f38b
sorapark <srnice5298@gmail.com>
2013-09-26 19:54:18 +0900

    bug fix

--- src/main/java/com/louddoor/hbase_streaming/JsonSerializer.java
+++ src/main/java/com/louddoor/hbase_streaming/JsonSerializer.java
@@ -19,10 +19,13 @@ public class JsonSerializer extends Serializer {
 
 	private JsonFactory f = new JsonFactory();
 	private JsonGenerator jg = null;
+  private OutputStreamWriter outputStreamWriter = null;
 
-	public JsonSerializer(OutputStream out) {
+  public JsonSerializer(OutputStream out) {
 		super(out);
 		try {
+
+      outputStreamWriter = new OutputStreamWriter(out);
 			jg = f.createJsonGenerator(new OutputStreamWriter(out));
 		} catch (IOException e) {
 			e.printStackTrace();
@@ -33,34 +36,39 @@ public class JsonSerializer extends Serializer {
 	public void writeMap(ImmutableBytesWritable rowKey, Result in) throws Exception {
 		Iterator<Entry<byte[], NavigableMap<byte[], byte[]>>> i = in.getNoVersionMap().entrySet().iterator();
 
-	//	jg.writeRaw(Bytes.toString(rowKey.get()) + "\t");
-    jg.writeRaw(Bytes.toString(rowKey.get()) );
-		jg.writeStartObject();
+    final String s = "user1_1\t{'details':{'page':'/'}}";
 
-		while(i.hasNext())
-		{
-			Entry<byte[], NavigableMap<byte[], byte[]>> ent =  i.next();
+    outputStreamWriter.write(s);
+    outputStreamWriter.flush();
 
-			jg.writeObjectFieldStart(Bytes.toString(ent.getKey()));
-
-			Map<byte[], byte[]> inner = ent.getValue();
-			Iterator<Entry<byte[], byte[]>> j = inner.entrySet().iterator();
-
-			while(j.hasNext())
-			{
-				Entry<byte[], byte[]> innerEnt = (Entry<byte[], byte[]>) j.next();
-
-				jg.writeStringField(Bytes.toString(innerEnt.getKey()), Bytes.toString(innerEnt.getValue()));
-			}
-
-			jg.writeEndObject();
-		}
-
-		jg.writeEndObject();
-
-		jg.writeRaw("\n");
-
-		jg.flush();
+//		jg.writeRaw(Bytes.toString(rowKey.get()) + "\t");
+////    jg.writeRaw(Bytes.toString(rowKey.get()) );
+//		jg.writeStartObject();
+//
+//		while(i.hasNext())
+//		{
+//			Entry<byte[], NavigableMap<byte[], byte[]>> ent =  i.next();
+//
+//			jg.writeObjectFieldStart(Bytes.toString(ent.getKey()));
+//
+//			Map<byte[], byte[]> inner = ent.getValue();
+//			Iterator<Entry<byte[], byte[]>> j = inner.entrySet().iterator();
+//
+//			while(j.hasNext())
+//			{
+//				Entry<byte[], byte[]> innerEnt = (Entry<byte[], byte[]>) j.next();
+//
+//				jg.writeStringField(Bytes.toString(innerEnt.getKey()), Bytes.toString(innerEnt.getValue()));
+//			}
+//
+//			jg.writeEndObject();
+//		}
+//
+//		jg.writeEndObject();
+//
+//		jg.writeRaw("\n");
+//
+//		jg.flush();
 	}
 
 	@Override
